987395112c40536186cc4d72e61a20485c69f660
Albert Yip <albertkaiyip+github@gmail.com>
2016-12-03 18:20:21 -0800

    webview done

--- app/src/main/java/com/example/mayeshakabir/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mayeshakabir/myapplication/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.mayeshakabir.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.webkit.*;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new MyWebViewClient());
+
+        webView.getSettings().setJavaScriptEnabled(true);
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    public void goToFacebook (View view) {
+        webView.loadUrl("https://www.facebook.com");
+        Uri uri = Uri.parse("https://www.facebook.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
+
