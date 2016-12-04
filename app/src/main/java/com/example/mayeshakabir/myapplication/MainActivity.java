25cd7a63404f3825dbfcd3721201a1604ee8318d
Albert Yip <albertkaiyip+github@gmail.com>
2016-12-03 18:41:00 -0800

    Linked buttons to open to corresponding site

--- app/src/main/java/com/example/mayeshakabir/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mayeshakabir/myapplication/MainActivity.java
@@ -40,5 +40,40 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
         startActivity(intent);
     }
+
+    public void gotToTwitter (View view) {
+        webView.loadUrl("https://www.twitter.com");
+        Uri uri = Uri.parse("https://www.twitter.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void goToInsta (View view) {
+        webView.loadUrl("https://www.instagram.com");
+        Uri uri = Uri.parse("https://www.instagram.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void goToYoutube (View view) {
+        webView.loadUrl("https://www.youtube.com");
+        Uri uri = Uri.parse("https://www.youtube.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void goToVimeo (View view) {
+        webView.loadUrl("https://www.vimeo.com");
+        Uri uri = Uri.parse("https://www.vimeo.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void goToLinkd (View view) {
+        webView.loadUrl("https://www.linkedin.com");
+        Uri uri = Uri.parse("https://www.linkedin.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
 }
 
